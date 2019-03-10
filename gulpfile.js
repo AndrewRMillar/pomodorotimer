5a65f6c77a5e2d53ed21c5b69c6472ca0d67c128
Andrew Millar <duckman.andrew@gmail.com>
2019-03-10 13:00:37 +0100

    Add changes to the gulp file, autoprefixer/line-ending-correction

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,36 @@ const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
-const browserSync  = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const lec = require('gulp-line-ending-corrector');
 
 // var pipeline = require('readable-stream').pipeline; // getting error
 
-// Copy all relavant files
-gulp.task('copyfiles', function(done) {
-  gulp.src(['/src/*.svg', './src/*.ico', './src/*.png', './src/*.html', './src/site.webmanifest', './src/*.xml'])
+// Copy all image files from base dir
+gulp.task('copyFav', function(done) {
+  gulp.src(['./src/*.ico', './src/*.png'])
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
   done();
 });
 
+// Copy all the text files from base dir
+gulp.task('copyfiles', function(done) {
+  gulp.src(['/src/*.svg', './src/*.html', './src/site.webmanifest', './src/*.xml'])
+    .pipe(lec())
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+  done();
+});
+
+
 // Optimize images
 gulp.task('imagemin', function(done) {
   gulp.src('src/img/*.*')
     .pipe(imagemin())
+    .pipe(lec())
     .pipe(gulp.dest('dist/img'))
     .pipe(browserSync.stream());
   done();
@@ -38,9 +51,14 @@ gulp.task('copysounds', function(done) {
 gulp.task('minify-css', function(done) {
   gulp.src('src/css/*.css')
     .pipe(concat('/css/styles.css'))
-    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(sourcemaps.write())
+    .pipe(lec())
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
   done();
@@ -49,13 +67,14 @@ gulp.task('minify-css', function(done) {
 // Transpile/Concat/uglify js files
 gulp.task('minify-js', function(done) {
   gulp.src('src/js/*.js')
-    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(babel({
       presets: ['@babel/env']
     }))
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write())
+    .pipe(lec())
     .pipe(gulp.dest('dist/js/'))
     .pipe(browserSync.stream());
   done();
@@ -74,4 +93,4 @@ gulp.task('watch', function() {
   gulp.watch('src/css/*.css', gulp.parallel('minify-css')).on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('copyfiles', 'copysounds', 'imagemin', 'minify-css', 'minify-js'));
+gulp.task('default', gulp.series('copyfiles', 'copyFav', 'copysounds', 'imagemin', 'minify-css', 'minify-js'));
